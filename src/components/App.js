63997bad5404b35f34d223ea4ea91d7be3f3ccfc
xyieko <emmanuel.ontweka@student.moringaschool.com>
2025-05-01 12:59:21 +0300

    src/components/App.js and src/index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+      })
+      .catch((error) => {
+        console.error("Error fetching dog image:", error);
+      });
+  }, []);
+
+  if (!dogImage) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={dogImage} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
+
